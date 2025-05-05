8d8f33462b0ab4f1d961dac566137ccee890683c
ClintonNdeh <cruzlook50@hotmail.com>
2025-05-05 11:18:20 +0200

    Create init_database.sql
    Extracted without recovery saved

---
+++ Scripts/init_database.sql
@@ -0,0 +1,11 @@
+USE [Datawarehouse]
+GO
+/****** Object:  Schema [bronze]    Script Date: 5/5/2025 11:12:19 AM ******/
+CREATE SCHEMA [bronze]
+GO
+/****** Object:  Schema [gold]    Script Date: 5/5/2025 11:12:19 AM ******/
+CREATE SCHEMA [gold]
+GO
+/****** Object:  Schema [silver]    Script Date: 5/5/2025 11:12:19 AM ******/
+CREATE SCHEMA [silver]
+GO
